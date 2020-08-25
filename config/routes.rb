698b9cbb2226756aaa85ab230bc90d6b3351b9d1
ryotasykt <ryotasykt@gmail.com>
2020-08-26 01:37:35 +0900

    既読機能のフロントエンド実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'posts#index' #←編集
 # get 'posts/new', to: 'posts#new' ←削除
   post 'posts', to: 'posts#create'
-  get 'posts', to: 'post#checked'
+  # get 'posts', to: 'post#checked'
+  get 'posts/:id', to: 'posts#checked'
 end
